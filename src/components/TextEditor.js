eded442e0ebc71911102770bbe44f056de6aa803
Vinay <itsvinayadav@gmail.com>
2022-05-09 08:59:13 +0530

    Moved Ajax call for image inside return

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -7,31 +7,6 @@ import htmlToDraft from 'html-to-draftjs';
 import './TextEditor.css';
 
 
-
-function uploadImageCallBack(file) {
-    return new Promise(
-        (resolve, reject) => {
-            const xhr = new XMLHttpRequest();
-            xhr.open('POST', 'https://api.imgur.com/3/image');
-            xhr.setRequestHeader('Authorization', 'Client-ID 2533e08d67ce1c7');
-            const data = new FormData();
-            data.append('image', file);
-            xhr.send(data);
-            xhr.addEventListener('load', () => {
-                const response = JSON.parse(xhr.responseText);
-                console.log(response)
-                resolve(response);
-            });
-            xhr.addEventListener('error', () => {
-                const error = JSON.parse(xhr.responseText);
-                console.log(error)
-                reject(error);
-            });
-        }
-    );
-}
-
-
 export default class TextEditor extends Component {
     constructor(props) {
         super(props);
@@ -67,10 +42,34 @@ export default class TextEditor extends Component {
     }
 
 
-
     render() {
-
         const { editorState } = this.state;
+
+        function uploadImageCallBack(file) {
+            return new Promise(
+                (resolve, reject) => {
+                    const xhr = new XMLHttpRequest();
+                    xhr.open('POST', 'https://api.imgur.com/3/image');
+                    xhr.setRequestHeader('Authorization', 'Client-ID 2533e08d67ce1c7');
+                    const data = new FormData();
+                    debugger
+                    console.log(file);
+                    data.append('image', file);
+                    xhr.send(data);
+                    xhr.addEventListener('load', () => {
+                        const response = JSON.parse(xhr.responseText);
+                        console.log(response)
+                        resolve(response);
+                    });
+                    xhr.addEventListener('error', () => {
+                        const error = JSON.parse(xhr.responseText);
+                        console.log(error)
+                        reject(error);
+                    });
+                }
+            );
+        }
+
         return <div className='editor'>
             <Editor
                 editorState={editorState}
@@ -81,11 +80,10 @@ export default class TextEditor extends Component {
                     textAlign: { inDropdown: true },
                     link: { inDropdown: true },
                     history: { inDropdown: true },
-                    image: { uploadCallback: uploadImageCallBack, alt: { present: true, mandatory: true } },
+                    image: { uploadCallback: uploadImageCallBack, alt: { present: true, mandatory: false } },
                     // image: { inDropdown: true },
                 }}
             />
-            {/* <button onClick={this.handleSubmit} className="btn-large waves-effect waves-light xbutton">Save</button> */}
         </div>
     }
 }
